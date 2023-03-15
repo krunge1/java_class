fdaacb30fc40e7360aca778a73e3e514a5be2e37
Kyle Runge <krunge1@hotmail.com>
2023-03-14 19:09:18 -0500

    test

--- Test.java
+++ Test.java
@@ -1,8 +1,8 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("My name is Kyle Runge");
-        System.out.println("I am 36 years old");
-        System.out.println("My hometown is Troy, IL");
+
+
     }
 }
 // This is test for github
